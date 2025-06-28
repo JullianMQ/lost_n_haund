847e2e1d8535ee25b94cee4a9b2998dc2e91448e
Jullian Quiambao <jullianmq@gmail.com>
2025-06-28 22:14:08 +0800

    added flutter client

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lost_n_haund
+
+A flutter project for lost n found items in HAU.
+
+
---
+++ lost_n_haund_client/android/app/src/main/kotlin/com/example/lost_n_haund_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lost_n_haund_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
