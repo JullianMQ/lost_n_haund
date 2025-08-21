5d3f52c7fae21a1c4f9444f7d9b0a116357dd294
Jero Halili <127023985+JeroH20@users.noreply.github.com>
2025-08-21 13:31:17 +0800

    Comment login-page

--- lost_n_haund_client/lib/main.dart
+++ lost_n_haund_client/lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:lost_n_haund_client/pages/login_page.dart';
+// import 'package:lost_n_haund_client/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      // home: LoginPage(),
     );
   }
 }
